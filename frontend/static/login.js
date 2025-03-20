823dcf5d3775c53c3bf672f4d98e200c0a5699fe
nyagooh <annmaina614@gmail.com>
2025-03-20 23:08:02 +0300

    add static file that is signup and login page

---
+++ frontend/static/login.js
@@ -0,0 +1,52 @@
+class LoginForm extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    const container = document.createElement("div");
+    container.classList.add("login-container");
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "/static/login.css";
+
+    container.innerHTML = `
+        <div class="left-section">
+             <h2>Capturing Moments, Creating Memories</h2>
+                    </div>
+         <div class="right-section">
+                        <h2>Welcome Back</h2>
+                        <p>Please Enter your Account details</p>
+                            <input type="email" placeholder="Email">
+                            <input type="password" placeholder="Enter your password">
+                           <div class="terms">
+                                <a href="#">Forgot Password?</a>
+                            </div>
+                            <button type="submit">Login</button>
+                        </form>
+                        <p>Or register with</p>
+                        <button id="login-btn" class="social-btn">
+                        <text class = "google-text" x="10" y="30" font-family="Poppins, Arial, sans-serif"  font-weight="bold">
+                                <span class="blue">G</span>
+                                <span class="red">o</span>
+                                <span class="yellow">o</span>
+                                <span class="blue">g</span>
+                                <span class="green">l</span>
+                                <span class="red">e</span>
+                        </text>
+                        </button>
+                     <div class="create-account">
+                  <p>Don't have an account? <a href="/signup">Create an account</a></p>
+                </div>
+         </div>
+
+        `;
+
+    this.shadowRoot.append(link, container);
+    this.shadowRoot.querySelector("#login-btn")
+      .addEventListener("click", () => {
+        window.location.href = "/auth/google";
+      });
+  }
+}
+customElements.define("login-form", LoginForm);
---
+++ frontend/static/signup.js
@@ -0,0 +1,56 @@
+class SignupForm extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    const container = document.createElement("div");
+    container.classList.add("signup-container");
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "/static/styles.css";
+
+    container.innerHTML = `
+              <div class="left-section">
+                          <h2>Capturing Moments, Creating Memories</h2>
+                      </div>
+                      <div class="right-section">
+                          <h2>Create an account</h2>
+                          <p>Already have an account? <a href="/login">Log in</a></p>
+                          <form>
+                              <div class="name-fields">
+                                  <input type="text" placeholder="First Name">
+                                  <input type="text" placeholder="Last Name">
+                              </div>
+                              <input type="email" placeholder="Email">
+                              <input type="password" placeholder="Enter your password">
+                              <div class="terms">
+                                  <input type="checkbox" id="terms">
+                                  <label for="terms">I agree to the <a href="#">Terms & Conditions</a></label>
+                              </div>
+                              <button type="submit">Create Account</button>
+                          </form>
+                          <p>Or register with</p>
+                          <button id="google-signup-btn" class="google-btn">
+                          <text class = "google-text" x="10" y="30" font-family="Poppins, Arial, sans-serif"  font-weight="bold">
+                                  <span class="blue">G</span>
+                                  <span class="red">o</span>
+                                  <span class="yellow">o</span>
+                                  <span class="blue">g</span>
+                                  <span class="green">l</span>
+                                  <span class="red">e</span>
+                          </text>
+                          </button>
+                      </div>
+          `;
+
+    this.shadowRoot.append(link, container);
+    this.shadowRoot
+      .querySelector("#google-signup-btn")
+      .addEventListener("click", () => {
+        window.location.href("/auth/google");
+      });
+  }
+}
+
+customElements.define("signup-form", SignupForm);
