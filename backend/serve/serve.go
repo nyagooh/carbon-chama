c018945d3e7c096cd5514287183132034b14f673
nyagooh <annmaina614@gmail.com>
2025-03-20 23:11:21 +0300

    add a function to serve html files

---
+++ backend/serve/serve.go
@@ -0,0 +1,36 @@
+package serve
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
+
+func ServePage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "methhod not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var page string
+	switch r.URL.Path {
+	case "/signup":
+		page = "signup.html"
+	case "/login":
+		page = "login.html"
+	case "/":
+		page = "login.html"
+	default:
+		http.NotFound(w, r)
+		return
+	}
+
+	tmplpath := filepath.Join("frontend", "templates", page)
+	tmpl, err := template.ParseFiles(tmplpath)
+	if err != nil {
+		fmt.Println("Hello Anne You finally found me!!")
+		http.Error(w, "internalservererror", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+}
