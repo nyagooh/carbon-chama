5bfc02f9cbc243ac00ba9f59844074ed6e483328
nyagooh <annmaina614@gmail.com>
2025-03-21 18:15:21 +0300

    edit script to be executable

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Download dependencies
 go mod download
-# Build the app into an executable named 'carbon-chama'
 go build -o carbon-chama .
+chmod +x carbon-chama  # Make it executable
